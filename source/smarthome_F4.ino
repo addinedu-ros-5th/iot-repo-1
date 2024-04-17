f6508dd0560c6ce1b63a6fb17b37928555bf896e
1yohanyo1 <163791820+1yohanyo1@users.noreply.github.com>
2024-04-17 22:53:27 +0900

    Optimizing the loop function

--- source/smarthome_F4.ino
+++ source/smarthome_F4.ino
@@ -1,33 +1,27 @@
-/* library */
+/* 라이브러리 */
 #include <SPI.h>
 #include <MFRC522.h>
-#include <List.hpp>
 #include <Servo.h>
 #include <DFRobot_DHT11.h>
-//////////////
+#include <List.hpp>
 
-/* pin information */
+/* 핀 정보 */
 const int R_LED = 2;
 const int G_LED = 3;
-// const int B_LED = 4;
 const int buzzer = 4;
-const int dustFan = A2; // replace air cleaner with fan
+const int dustFan = A2;
 const int dustSensor = A3;
 const int MQ135 = A4;
 const int MQ2 = A5;
-
 const int BUTTON = 5;
 const int PIR = 6;
 const int RST_PIN = 9;
 const int SS_PIN = 10;
-
 const int airFan = 7;
 const int DHT11_PIN = A1;
-
 const int servo_pin = 8; 
 
-/* variable */
-
+/* 변수 */
 int cardDetected = false;
 int pirstate = false;
 int person_count = 0;
@@ -39,13 +33,11 @@ int humidity;
 DFRobot_DHT11 DHT;
 
 bool fan_on = false;
-
 int cutain_flag = 1;
 Servo servo;
 unsigned long lightstart = 0;
 unsigned long lightclose = 0;
 const unsigned long lightdelay = 3000;
-
 const int COLOR[][3] = {
     {200, 0, 0},    // Red
     {200, 150, 0},  // Orange
@@ -53,14 +45,11 @@ const int COLOR[][3] = {
     {0, 200, 0},    // Green
     {0, 0, 200}     // Blue
 };
-
 int dustLevel = 0;
-int gasLevel =0 ;
-int flammableGasLevel =0;
+int gasLevel = 0;
+int flammableGasLevel = 0;
 String quality = "";
-
-int sensorThres = 200; // flammableGas 기준 변경 가능
-
+int sensorThres = 200;
 
 typedef enum {
   REGISTRATION = 0,
@@ -69,182 +58,113 @@ typedef enum {
 
 MFRC522 rc522(SS_PIN, RST_PIN);
 RFID_STATUS rfid_status;
-// 등록된 카드를 저장할 tag_list
 List<MFRC522::Uid> tag_list;
 
+/* 함수 선언 */
+void detectMotion();
+void countPeople();
+void controlVentilation();
+void controlCurtain();
+void readTemperatureHumidity();
+void manualControlCurtain();
 
-/* function */
-int buttonPress() {
-  int press;
-  int buttonState;
-  static int prevButtonState = HIGH;
-  buttonState = digitalRead(BUTTON);
-  press = (buttonState == LOW && prevButtonState == HIGH);
-  prevButtonState = buttonState;
-  return press;
-}
-
-void printUID(MFRC522::Uid uid) {
-  for (byte i = 0; i < 4; i++) {
-    Serial.print(uid.uidByte[i] < 0x10 ? " 0" : " ");
-    Serial.print(uid.uidByte[i], HEX);
-  }
-  Serial.println();
-}
-
-bool checkUID(MFRC522::Uid uid) {
-  for (int i = 0; i < tag_list.getSize(); i++) {
-    if (memcmp(tag_list.get(i).uidByte, rc522.uid.uidByte, 4) == 0) {
-      return true;
-    }
-  }
-  return false;
-}
-
-void air_sensor(){
-   dustLevel = analogRead(dustSensor);
-   gasLevel = analogRead(MQ135);
-  if(dustLevel<150){
-    quality = "Very GOOD!";
-     analogWrite(R_LED, COLOR[4][0]);
-     analogWrite(G_LED, COLOR[4][1]);
-    //analogWrite(B_LED, COLOR[4][2]);
-     analogWrite(dustFan, 0);
-  }
-   else if(dustLevel<300){
-     quality = "GOOD!";
-     analogWrite(R_LED, COLOR[3][0]);
-     analogWrite(G_LED, COLOR[3][1]);
-    //analogWrite(B_LED, COLOR[3][2]);
-     analogWrite(dustFan, 0);
-   }
-   else if (dustLevel<450){
-     quality = "Poor!";
-     analogWrite(R_LED, COLOR[2][0]);
-     analogWrite(G_LED, COLOR[2][1]);
-    //analogWrite(B_LED, COLOR[2][2]);
-     analogWrite(dustFan, 150);
-   }
-   else if (dustLevel<600){
-     quality  = "Very bad!";
-     analogWrite(R_LED, COLOR[1][0]);
-     analogWrite(G_LED, COLOR[1][1]);
-    //analogWrite(B_LED, COLOR[1][2]);
-     analogWrite(dustFan, 150);
-   }
-   else{
-     quality = "Toxic";
-     analogWrite(R_LED, COLOR[0][0]);
-     analogWrite(G_LED, COLOR[0][1]);
-    //analogWrite(B_LED, COLOR[0][2]);
-     analogWrite(dustFan, 150);
-   }
-  Serial.print("dust level : ");
-  Serial.println(dustLevel);
-  // Serial.println(quality);
-  Serial.print("gas level : ");
-  Serial.println(gasLevel);
-}
-
-void buzz_operation(){
-  flammableGasLevel = analogRead(MQ2);
-  Serial.print("flammableGasLevel : ");
-  Serial.println(flammableGasLevel);
-  // Checks if it has reached the threshold value
-  if (flammableGasLevel > sensorThres){
-    tone(buzzer, 1000, 200);
-  }
-  else{
-    noTone(buzzer);
-  }
-}
-
-void control_fan_based_on_humidity() {
-  if (DHT.humidity <= 40 || DHT.humidity >= 50) {
-    digitalWrite(airFan, HIGH);
-    fan_on = true;
-  } else if (fan_on) {
-    digitalWrite(airFan, LOW);
-    fan_on = false;
-  }
-}
-
-void read_sensor_data() {
-  DHT.read(DHT11_PIN);
-  auto_temperature = DHT.temperature;
-  humidity = DHT.humidity;
-
-  Serial.print("temperature : ");
-  Serial.print(auto_temperature);
-  Serial.print(" , ");
-  Serial.print("humidity : ");
-  Serial.println(humidity);
-  Serial.println();
-}
-
-void control_fan() {
-  if (auto_temperature == 27) {
-    unsigned long currentMillis = millis();
-    if (currentMillis - previousMillis >= 3000) {
-      analogWrite(airFan, 0);
-      fan_on = false;
-    }
-  } else {
-    analogWrite(airFan, 150);
-    fan_on = true;
-  }
-}
-
-void control_fan_gui() {
-  if (Serial.available() > 0) {
-    char command = Serial.read();
-    if (command == '1') {
-      analogWrite(airFan, 150);
-      fan_on = true;
-    } else if (command == '0') {
-      analogWrite(airFan, 0);
-      fan_on = false;
-    }
-  }
-}
-
-/* set up */ 
+/* 설정 */
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
   SPI.begin();
   rc522.PCD_Init();
   rfid_status = RFID_STATUS::REGISTRATION;
-  Serial.println("[STATUS] Registration");
   pinMode(BUTTON, INPUT_PULLUP);
   pinMode(PIR, INPUT);
-
   pinMode(airFan, OUTPUT);
   servo.attach(servo_pin);
   servo.write(10);
-
   pinMode(dustSensor, INPUT);
   pinMode(MQ2, INPUT);
   pinMode(MQ135, INPUT);
   pinMode(buzzer, OUTPUT);
-
 }
 
-/* loop */
+/* 루프 */
 void loop() {
-  air_sensor();
-  buzz_operation();
+  detectMotion();
+  countPeople();
+  controlVentilation();
+  controlCurtain();
+  readTemperatureHumidity();
+  manualControlCurtain();
+}
 
-  unsigned long currentMillis = millis();  // 현재 시간을 가져옵니다.
-
-  if (currentMillis - previousMillis >= interval) {
-    previousMillis = currentMillis;  // 이전 시간을 업데이트합니다.
-    read_sensor_data();
-    control_fan();
+/* 함수 구현 */
+void detectMotion() {
+  int value = digitalRead(PIR);
+  if (value) {
+    pirstate = true;
+    flag = true;
   }
-  control_fan_gui();
+}
 
+void countPeople() {
+  if (rfid_status == RFID_STATUS::VERIFICATION && cardDetected == true) {
+    if (pirstate) {
+      person_count--;
+      cardDetected = false;
+      pirstate = false;
+      flag = false;
+      if (person_count < 0) {
+        person_count = 0;
+      }
+    }
+    while (rfid_status == RFID_STATUS::VERIFICATION && cardDetected == true && pirstate == false && flag == true) {
+      int value = digitalRead(PIR);
+      if (value) {
+        person_count++;
+        cardDetected = false;
+        pirstate = false;
+        flag = false;
+        delay(100);
+        break;
+      }
+    }
+  }
+}
 
+void controlVentilation() {
+  dustLevel = analogRead(dustSensor);
+  gasLevel = analogRead(MQ135);
+  if (dustLevel < 150) {
+    quality = "Very GOOD!";
+    analogWrite(R_LED, COLOR[4][0]);
+    analogWrite(G_LED, COLOR[4][1]);
+    analogWrite(dustFan, 0);
+  } else if (dustLevel < 300) {
+    quality = "GOOD!";
+    analogWrite(R_LED, COLOR[3][0]);
+    analogWrite(G_LED, COLOR[3][1]);
+    analogWrite(dustFan, 0);
+  } else if (dustLevel < 450) {
+    quality = "Poor!";
+    analogWrite(R_LED, COLOR[2][0]);
+    analogWrite(G_LED, COLOR[2][1]);
+    analogWrite(dustFan, 150);
+  } else if (dustLevel < 600) {
+    quality = "Very bad!";
+    analogWrite(R_LED, COLOR[1][0]);
+    analogWrite(G_LED, COLOR[1][1]);
+    analogWrite(dustFan, 150);
+  } else {
+    quality = "Toxic";
+    analogWrite(R_LED, COLOR[0][0]);
+    analogWrite(G_LED, COLOR[0][1]);
+    analogWrite(dustFan, 150);
+  }
+  Serial.print("dust level : ");
+  Serial.println(dustLevel);
+  Serial.print("gas level : ");
+  Serial.println(gasLevel);
+}
+
+void controlCurtain() {
   int light = analogRead(A0);
   int scaledLight = map(light, 0, 1023, 0, 100);
   Serial.println(scaledLight);
@@ -272,120 +192,32 @@ void loop() {
       }
     }
     if (Serial.available() > 0) {
-      int angle = Serial.parseInt();  // 시리얼 포트로부터 각도를 읽음
-      servo.write(angle);             // 서보모터를 해당 각도로 회전
+      int angle = Serial.parseInt();
+      servo.write(angle);
     }
   }
+}
 
-
-  ///////////////////////////////////////////////////////////////////////////////////
-
-  Serial.print("1번");
-  DHT.read(DHT11_PIN);  // 만악의 근원
-  Serial.print("2번");
-  int temperature = DHT.temperature;
-  int humidity = DHT.humidity;
-  Serial.print(temperature);
-  Serial.print(",");
+void readTemperatureHumidity() {
+  DHT.read(DHT11_PIN);
+  auto_temperature = DHT.temperature;
+  humidity = DHT.humidity;
+  Serial.print("temperature : ");
+  Serial.print(auto_temperature);
+  Serial.print(" , ");
+  Serial.print("humidity : ");
   Serial.println(humidity);
+}
 
-  // if (DHT.temperature <= 26 || DHT.temperature >= 28)
-  if (DHT.humidity <= 40 || DHT.humidity >= 50) {
-    digitalWrite(airFan, HIGH);
-    fan_on = true;
-  } else if (fan_on) {
-    digitalWrite(airFan, LOW);
-    fan_on = false;
-  }
+void manualControlCurtain() {
   if (Serial.available() > 0) {
     char command = Serial.read();
     if (command == '1') {
-      digitalWrite(airFan, HIGH);
+      analogWrite(airFan, 150);
       fan_on = true;
     } else if (command == '0') {
-      digitalWrite(airFan, LOW);
+      analogWrite(airFan, 0);
       fan_on = false;
     }
   }
-  control_fan_based_on_humidity();
-
-
-
-  // put your main code here, to run repeatedly:
-  int value = digitalRead(PIR);
-
-  // Serial.println(value);
-
-  if (buttonPress() == true) {
-    if (rfid_status == RFID_STATUS::REGISTRATION) {
-      rfid_status = RFID_STATUS::VERIFICATION;
-      Serial.println("[STATUS] Verification");
-    } else {
-      rfid_status = RFID_STATUS::REGISTRATION;
-      Serial.println("[STATUS] Registration");
-    }
-  }
-  if (!rc522.PICC_IsNewCardPresent()) {
-    return;
-  }
-  if (!rc522.PICC_ReadCardSerial()) {
-    return;
-  }
-  if (rfid_status == RFID_STATUS::REGISTRATION) {
-    if (checkUID(rc522.uid) == false) {
-      tag_list.addLast(rc522.uid);
-      Serial.print("Registration Tag : ");
-      tag_list.addLast(rc522.uid);
-      printUID(rc522.uid);
-    }
-  }
-  if (checkUID(rc522.uid)) {
-    cardDetected = true;
-  } else {
-    cardDetected = false;
-  }
-  if (value) {
-    pirstate = true;
-    flag = true;
-  } else {
-  }
-  if (rfid_status == RFID_STATUS::VERIFICATION && cardDetected == true) {  // rfid_status == VERIFICATION
-    if (pirstate) {
-
-      person_count--;
-      cardDetected = false;
-      pirstate = false;
-      flag = false;
-      // Serial.print("감소: ");
-      // Serial.println(person_count);
-      if (person_count < 0) {
-        person_count = 0;
-      }
-      delay(100);
-    }
-    // Serial.println(flag);
-    while (rfid_status == RFID_STATUS::VERIFICATION && cardDetected == true && pirstate == false && flag == true) {
-      Serial.println("while문 입장");
-      int value = digitalRead(PIR);
-      if (value) {
-        Serial.print("pir 값: ");
-        Serial.println(pirstate);
-        person_count++;
-        cardDetected = false;
-        pirstate = false;
-        flag = false;
-        // Serial.print("증가: ");
-        // Serial.println(person_count);
-        delay(100);
-        break;
-      }
-    }
-    Serial.println("탈출");
-  }
-  delay(100);
-  Serial.print("인원 수 :");
-  Serial.println(person_count);
-  flag = true;
 }
-
-
