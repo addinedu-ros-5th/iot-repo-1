db11213cd7d3251508377afaf911fe5018f8aae5
1yohanyo1 <163791820+1yohanyo1@users.noreply.github.com>
2024-04-22 09:44:33 +0900

    Update care_pc1.ino

--- source/care_pc1.ino
+++ source/care_pc1.ino
@@ -4,62 +4,34 @@
 
 const int RST_PIN = 9;
 const int SS_PIN = 10;
-typedef enum
-{
+typedef enum {
   REGISTRATION = 0,
-  VERIFICATION
 } RFID_STATUS;
-MFRC522 rc522(SS_PIN, RST_PIN);
+MFRC522 rfid(SS_PIN, RST_PIN);  // MFRC522 instance 생성
 RFID_STATUS rfid_status;
-List<MFRC522::Uid> tag_list;
 
-void printUID(MFRC522::Uid uid)
-{
-  for (byte i = 0; i < 4; i++)
-  {
-    Serial.print(uid.uidByte[i] < 0x10 ? " 0" : " ");
-    Serial.print(uid.uidByte[i], HEX);
-  }
-  Serial.println();
-}
-bool checkUID(MFRC522::Uid uid)
-{
-  for (int i = 0; i < tag_list.getSize(); i++)
-  {
-    if (memcmp(tag_list.get(i).uidByte, rc522.uid.uidByte, 4) == 0)
-    {
-      return true;
-    }
-  }
-  return false;
-}
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
 
   SPI.begin();
-  rc522.PCD_Init();
+  rfid.PCD_Init();
   rfid_status = RFID_STATUS::REGISTRATION;
   // Serial.println("[STATUS] Registration");
 }
 void loop() {
   //
-  
-  if (!rc522.PICC_IsNewCardPresent()) {
-    return;
-  }
-  if (!rc522.PICC_ReadCardSerial()) {
-    return;
-  }
-  if (rfid_status == RFID_STATUS::REGISTRATION)
-  {
-    if (checkUID(rc522.uid) == false)
-    {
-      tag_list.addLast(rc522.uid);
-      // tag_list.addLast(rc522.uid);
-      printUID(rc522.uid);
-    }
-  }
 
-  delay(100);
+  if (rfid.PICC_IsNewCardPresent() && rfid.PICC_ReadCardSerial()) {
+    // Print UID of the card
+    for (byte i = 0; i < rfid.uid.size; i++) {
+      Serial.print(rfid.uid.uidByte[i] < 0x10 ? "0" : "");  // leading zero 출력
+      Serial.print(rfid.uid.uidByte[i], HEX);
+      if (i + 1 < rfid.uid.size) {
+      }
+    }
+
+    delay(1000);
+    Serial.println();
+  }
 }
