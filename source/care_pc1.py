a493f0a1a7409b8cce6e503891dc149624247ce0
1yohanyo1 <163791820+1yohanyo1@users.noreply.github.com>
2024-04-22 15:11:23 +0900

    Update care_pc1.py

--- source/care_pc1.py
+++ source/care_pc1.py
@@ -1,36 +1,29 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QTextEdit, QVBoxLayout, QPushButton
-from PyQt5.QtCore import QTimer
-import serial
 import socket
 import json
+from PyQt5.QtCore import QTimer, QUrl
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWebEngineWidgets import QWebEngineView
+from PyQt5 import uic
+from PyQt5.QtGui import QDesktopServices
+import serial
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
-class SerialReceiver(QWidget):
+from_class = uic.loadUiType("care_pc1.ui")[0]
+
+class WindowClass(QMainWindow, from_class):
     def __init__(self):
         super().__init__()
+        self.setupUi(self)
 
         self.uids = [None, None]  # UID를 저장할 배열 초기화
-
-        self.initUI()
-
-    def initUI(self):
-        self.setGeometry(100, 100, 400, 300)
-        self.setWindowTitle('Serial Receiver')
-
-        layout = QVBoxLayout()
-
-        self.text_edit = QTextEdit()
-        layout.addWidget(self.text_edit)
-
-        # 전송 버튼 추가
-        self.send_button = QPushButton('Send UID to 2nd PC', self)
-        layout.addWidget(self.send_button)
-        self.send_button.clicked.connect(self.sendUID)
-
-        self.setLayout(layout)
-
         self.serial_port = serial.Serial('/dev/ttyACM0', 9600)  # 아두이노 포트에 따라 변경
 
+        self.btn_enter.clicked.connect(self.sendUID)
+        self.btn_clear.clicked.connect(self.clearFields)  # 수정 버튼과 연결
+        self.btn_call.clicked.connect(self.callEmergency)
         self.timer = QTimer(self)
         self.timer.timeout.connect(self.readData)
         self.timer.start(100)  # 100ms마다 데이터 수신 확인
@@ -53,7 +46,7 @@ class SerialReceiver(QWidget):
 
     def sendUID(self):
         # 두 번째 피시로 데이터 전송
-        HOST = '172.30.1.43'  # 두 번째 피시의 IP 주소
+        HOST = '192.168.0.67'  # 두 번째 피시의 IP 주소
         PORT = 12345           # 포트 번호
 
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -65,18 +58,67 @@ class SerialReceiver(QWidget):
                 s.sendall(packed_data)
                 print("UID data sent successfully!")
                 
-                # 화면 클리어 및 첫 번째 UID 출력
-                self.text_edit.clear()
-                self.text_edit.append(self.uids[0])
-                self.uids[1] = None
+                # 카드 정보를 입력 비활성화
+                self.text_edit.setReadOnly(True)
+                self.nameEdit.setReadOnly(False)
+                self.phoneEdit.setReadOnly(False)
+                self.addEdit.setReadOnly(False)
                 
             except Exception as e:
                 print("Failed to send UID data:", e)
 
+    def clearFields(self):
+        # 입력 필드 초기화 및 수정 가능하도록 변경
 
+        self.uids[1] = None
 
-if __name__ == '__main__':
+        self.text_edit.clear()
+        self.text_edit.setReadOnly(True)
+        self.nameEdit.clear()
+        self.nameEdit.setReadOnly(False)
+        self.phoneEdit.clear()
+        self.phoneEdit.setReadOnly(False)
+        self.addEdit.clear()
+        self.addEdit.setReadOnly(False)
+
+    def callEmergency(self):            
+        # 셀레니움을 사용하여 웹 브라우저를 열고 작업 수행
+        driver = webdriver.Chrome()  # Chrome 드라이버 사용
+        driver.get("https://www.119.go.kr/Center119/regist.do#")
+        
+        # 이름 입력
+        name = self.nameEdit.text()
+        name_field = driver.find_element(By.XPATH, '//*[@id="dsr_name"]')
+        name_field.send_keys(name)
+        time.sleep(1)
+        # 전화번호 입력
+        phone = self.phoneEdit.text()
+        phone1_field = driver.find_element(By.XPATH, '//*[@id="call_tel1"]')
+        phone1_field.send_keys(phone[:3])
+        phone2_field = driver.find_element(By.XPATH, '//*[@id="call_tel2"]')
+        phone2_field.send_keys(phone[3:7])
+        phone3_field = driver.find_element(By.XPATH, '//*[@id="call_tel3"]')
+        phone3_field.send_keys(phone[7:])
+        time.sleep(1)
+        # 체크박스 체크
+        check = driver.find_element(By.XPATH, '//*[@id="agree01"]')
+        check.click()
+        time.sleep(1)
+        # 주소 입력
+        address = self.addEdit.text()
+        address_field = driver.find_element(By.XPATH, '//*[@id="juso2"]')
+        address_field.send_keys(address)
+        time.sleep(1)
+        # 버튼 클릭
+        save_btn = driver.find_element(By.XPATH, '//*[@id="save_btn"]')
+        save_btn.click()
+        time.sleep(1)
+
+        # 작업이 완료되면 웹 브라우저를 닫습니다.
+        QTimer.singleShot(3000,  driver.quit)  # 3초 대기 후 브라우저 종료
+
+if __name__ == "__main__":
     app = QApplication(sys.argv)
-    window = SerialReceiver()
-    window.show()
+    myWindows = WindowClass()
+    myWindows.show()
     sys.exit(app.exec_())
